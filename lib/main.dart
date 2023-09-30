a396adf0cb0bff305cd9ff125fd18510d1675af7
Milson Feliciano <143576093+WinningSonn@users.noreply.github.com>
2023-10-01 00:15:56 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eCommerce int2',
+      title: 'Bad Tech',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
