2496e9a493b492d47cf666b3a281abe57f6e2fc2
Schino <20739822+schiWasTaken@users.noreply.github.com>
2023-10-04 20:01:24 +0700

    Update app_properties

--- lib/app_properties.dart
+++ lib/app_properties.dart
@@ -5,11 +5,17 @@ const Color mediumYellow = Color(0xffFDB846);
 const Color darkYellow = Color(0xffE99E22);
 const Color transparentYellow = Color.fromRGBO(253, 184, 70, 0.7);
 const Color darkGrey = Color(0xff202020);
+const Color navyBlue = Color(0xff000080);
+const Color black = Colors.black;
+const Color darkBackground = Color(0xff121212);
+const Color darkText = Colors.white;
+const Color darkIcon = Colors.white;
+const Color darkAccent = Color(0xff3498db);
 
 const LinearGradient mainButton = LinearGradient(colors: [
-  Color.fromRGBO(236, 60, 3, 1),
-  Color.fromRGBO(234, 60, 3, 1),
-  Color.fromRGBO(216, 78, 16, 1),
+  Color.fromRGBO(236, 54, 3, 1),
+  Color.fromRGBO(234, 53, 3, 1),
+  Color.fromRGBO(216, 89, 16, 1),
 ], begin: FractionalOffset.topCenter, end: FractionalOffset.bottomCenter);
 
 const List<BoxShadow> shadow = [
