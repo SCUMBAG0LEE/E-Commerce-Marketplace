29f5a3705f4feef08b8bc804e1e0abde7c90087b
Milson Feliciano <143576093+WinningSonn@users.noreply.github.com>
2023-10-05 23:48:47 +0700

    Add Map API

--- lib/screens/tracking_page.dart
+++ lib/screens/tracking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // Import the Google Maps package
 import 'package:bad_tech/models/product.dart';
 import 'package:bad_tech/screens/main/main_page.dart';
 
@@ -12,6 +13,7 @@ class TrackingPage extends StatefulWidget {
 
 class _TrackingPageState extends State<TrackingPage> {
   String selectedProduct = '';
+  GoogleMapController? mapController; // Add a controller for the Google Map
 
   @override
   void initState() {
@@ -24,10 +26,7 @@ class _TrackingPageState extends State<TrackingPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Create a Set to store unique product names
     final Set<String> uniqueProductNames = Set<String>();
-
-    // Filter out duplicates based on product name
     final List<Product> uniqueProducts = widget.selectedProducts.where((product) {
       final isUnique = uniqueProductNames.add(product.name);
       return isUnique;
@@ -48,111 +47,77 @@ class _TrackingPageState extends State<TrackingPage> {
         ),
         actions: <Widget>[],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  items: uniqueProducts
-                      .map((product) => DropdownMenuItem<String>(
-                            value: product.name, // Use a unique identifier as the value
-                            child: Text(
-                              product.name,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ))
-                      .toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      selectedProduct = newValue!;
-                    });
-                  },
-                  value: selectedProduct,
-                  isExpanded: true,
-                  icon: Icon(Icons.keyboard_arrow_down),
-                  elevation: 0,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(-6.165725, 106.791763), // Jakarta, Grogol near Tarumanagara University coordinates
+              zoom: 15.0, // You can adjust the initial zoom level
+            ),
+
+            markers: {
+              Marker(
+                markerId: MarkerId('tarumanagara_university'),
+                position: LatLng(-6.165725, 106.791763), // Tarumanagara University coordinates
+                infoWindow: InfoWindow(title: 'Tarumanagara University'),
+                icon: BitmapDescriptor.defaultMarker, // You can customize the icon if needed
                 ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.grey[100],
-              padding: EdgeInsets.all(16.0),
-              child: Image.asset(
-                'assets/map.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 60,
-            width: MediaQuery.of(context).size.width / 2,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.orange, Colors.deepOrange],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(0, 0, 0, 0.16),
-                  offset: Offset(0, 5),
-                  blurRadius: 10.0,
-                ),
-              ],
-              borderRadius: BorderRadius.circular(9.0),
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                // Navigate to MainPage using a MaterialPageRoute
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MainPage(),
-                  ),
-                );
               },
-              style: ButtonStyle(
-                elevation: MaterialStateProperty.all(0), // Remove button elevation
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(9.0),
-                  ),
-                ),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent), // Make button transparent
-              ),
-              child: Text(
-                'Go to Main Page',
-                style: TextStyle(
+            onMapCreated: (GoogleMapController controller) {
+              setState(() {
+                mapController = controller;
+              });
+            },
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    items: uniqueProducts.map((product) =>
+                        DropdownMenuItem<String>(
+                          value: product.name,
+                          child: Text(
+                            product.name,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        )).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedProduct = newValue!;
+                      });
+                    },
+                    value: selectedProduct,
+                    isExpanded: true,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    elevation: 0,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
               ),
             ),
