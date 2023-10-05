996561e324bf0aca6190fc10288150fc5f35c08f
William <68315131+SCUMBAG0LEE@users.noreply.github.com>
2023-10-05 20:48:16 +0700

    Update welcome_back_page.dart

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -1,27 +1,28 @@
 
 import 'package:bad_tech/app_properties.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'register_page.dart';
+import 'package:bad_tech/screens/main/main_page.dart';
 
-class                            WelcomeBackPage extends StatefulWidget {
+class WelcomeBackPage extends StatefulWidget {
   @override
   _WelcomeBackPageState createState() => _WelcomeBackPageState();
 }
 
-class _WelcomeBackPageState extends State<WelcomeBackPage> {
-  TextEditingController email =
-      TextEditingController(text: 'example@email.com');
+final _auth = FirebaseAuth.instance;
 
-  TextEditingController password = TextEditingController(text: '12345678');
+class _WelcomeBackPageState extends State<WelcomeBackPage> {
+  late String email;
+  late String password;
 
   @override
   Widget build(BuildContext context) {
     Widget welcomeBack = Text(
-      'Welcome Back Roberto,',
+      'Welcome To Bad Tech!',
       style: TextStyle(
           color: Colors.white,
-          fontSize: 34.0,
+          fontSize: 32.0,
           fontWeight: FontWeight.bold,
           shadows: [
             BoxShadow(
@@ -35,7 +36,7 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
     Widget subTitle = Padding(
         padding: const EdgeInsets.only(right: 56.0),
         child: Text(
-          'Login to your account using\nMobile number',
+          'Login to your account.',
           style: TextStyle(
             color: Colors.white,
             fontSize: 16.0,
@@ -46,9 +47,37 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
       left: MediaQuery.of(context).size.width / 4,
       bottom: 40,
       child: InkWell(
-        onTap: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (_) => RegisterPage()));
+        onTap: () async {
+          try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (_) => MainPage()));
+                      }
+                    } catch (e) {
+                      print(e);
+                      if (e is FirebaseAuthException) {
+                        if (e.code == 'INVALID_LOGIN_CREDENTIALS')
+                        { showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Error"),
+        content: Text("Wrong Email / Password."),
+        actions: <Widget>[
+          TextButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+              // You can add additional logic here if needed
+            },
+          ),
+        ],
+      );
+    },
+  ); }
+                      }
+                    }
         },
         child: Container(
           width: MediaQuery.of(context).size.width / 2,
@@ -100,16 +129,18 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                   child: TextField(
-                    controller: email,
+                    onChanged: (value) {email = value;},
                     style: TextStyle(fontSize: 16.0),
+                    decoration: InputDecoration(hintText: 'Email'),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                   child: TextField(
-                    controller: password,
+                    onChanged: (value) {password = value;},
                     style: TextStyle(fontSize: 16.0),
                     obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
                   ),
                 ),
               ],
@@ -121,22 +152,33 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
     );
 
     Widget forgotPassword = Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Row(
+  padding: const EdgeInsets.only(bottom: 20),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
-            'Forgot your password? ',
+            'Dont Have An Account Yet?',
             style: TextStyle(
               fontStyle: FontStyle.italic,
               color: Color.fromRGBO(255, 255, 255, 0.5),
               fontSize: 14.0,
             ),
           ),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
           InkWell(
-            onTap: () {},
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => RegisterPage()));
+            },
             child: Text(
-              'Reset password',
+              'Register',
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
@@ -146,7 +188,10 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
           ),
         ],
       ),
-    );
+    ],
+  ),
+);
+
 
     return Scaffold(
 
