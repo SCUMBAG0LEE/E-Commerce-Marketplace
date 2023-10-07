a26e0b3cc793240616ffedc484682177da84b223
Milson Feliciano <143576093+WinningSonn@users.noreply.github.com>
2023-10-08 01:39:40 +0700

    Adding navigator and pop up

--- lib/screens/payment/payment_page.dart
+++ lib/screens/payment/payment_page.dart
@@ -1,8 +1,9 @@
 import 'package:bad_tech/app_properties.dart';
 import 'package:bad_tech/screens/product/components/color_list.dart';
+import 'package:bad_tech/screens/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:math' as math;
+import 'dart:async'; // Import the async package
 
 class PaymentPage extends StatefulWidget {
   @override
@@ -22,23 +23,33 @@ class _PaymentPageState extends State<PaymentPage> {
   @override
   void initState() {
     super.initState();
-    scrollController.addListener(() {
-      if (scrollController.position.userScrollDirection.index == 1) {
-        FocusScope.of(context).requestFocus(FocusNode());
-      }
-    });
-  }
+     scrollController.addListener(() {
+    final direction = scrollController.position.userScrollDirection;
+    if (direction != null && direction.index == 1) {
+      FocusScope.of(context).requestFocus(FocusNode());
+    }
+  });
+}
 
+  //Format number card
   String convertCardNumber(String src, String divider) {
     String newStr = '';
     int step = 4;
-    for (int i = 0; i < src.length; i += step) {
-      newStr += src.substring(i, math.min(i + step, src.length));
-      if (i + step < src.length) newStr += divider;
+     for (int i = 0; i < src.length; i += step) {
+    final end = i + step;
+    if (end <= src.length) {
+      newStr += src.substring(i, end);
+      if (end < src.length) {
+        newStr += divider;
+      }
+    } else {
+      // Handle the case when the remaining part is shorter than 4 digits
+      newStr += src.substring(i);
     }
-    return newStr;
   }
-
+  return newStr;
+}
+  //Display month and year
   String convertMonthYear(String month, String year) {
     if (month.isNotEmpty)
       return month + '/' + year;
@@ -46,31 +57,75 @@ class _PaymentPageState extends State<PaymentPage> {
       return '';
   }
 
+  // Method to show the "Done!" dialog and navigate to profile page
+  void _showDoneDialogAndNavigateToProfile() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.green,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          title: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.done,
+              color: Colors.green,
+              size: 40,
+            ),
+            radius: 40,
+          ),
+          content: Text(
+            "Done!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+    );
+
+    // Delay the navigation to the profile page by 2 seconds
+    Timer(Duration(seconds: 2), () {
+      Navigator.of(context).pop();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget addThisCard = InkWell(
-//      onTap: () => Navigator.of(context)
-//          .push(MaterialPageRoute(builder: (_) => ViewProductPage())),
+      onTap: () {
+        // Show the "Done!" dialog and navigate to profile page
+        _showDoneDialogAndNavigateToProfile();
+      },
       child: Container(
         height: 80,
         width: MediaQuery.of(context).size.width / 1.5,
         decoration: BoxDecoration(
-            gradient: mainButton,
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.16),
-                offset: Offset(0, 5),
-                blurRadius: 10.0,
-              )
-            ],
-            borderRadius: BorderRadius.circular(9.0)),
+          gradient: mainButton,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.16),
+              offset: Offset(0, 5),
+              blurRadius: 10.0,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(9.0),
+        ),
         child: Center(
-          child: Text("Add This Card",
-              style: const TextStyle(
-                  color: const Color(0xfffefefe),
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 20.0)),
+          child: Text(
+            "Add This Card",
+            style: const TextStyle(
+              color: const Color(0xfffefefe),
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.normal,
+              fontSize: 20.0,
+            ),
+          ),
         ),
       ),
     );
@@ -111,8 +166,9 @@ class _PaymentPageState extends State<PaymentPage> {
                       width: MediaQuery.of(context).size.width,
                       padding: EdgeInsets.all(32.0),
                       decoration: BoxDecoration(
-                          color: active,
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                        color: active,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
@@ -129,36 +185,48 @@ class _PaymentPageState extends State<PaymentPage> {
                                 color: Colors.white,
                               ),
                               Flexible(
-                                  child: Center(
-                                      child: Text(
-                                          convertCardNumber(
-                                              cardNumber.text, '-'),
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18.0)))),
+                                child: Center(
+                                  child: Text(
+                                    convertCardNumber(cardNumber.text, '-'),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
                           Spacer(),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
-                              Text(convertMonthYear(month.text, year.text),
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold)),
-                              Text(cvc.text,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold))
+                              Text(
+                                convertMonthYear(month.text, year.text),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                cvc.text,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
                             ],
                           ),
                           Spacer(),
-                          Text(cardHolder.text,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0))
+                          Text(
+                            cardHolder.text,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -180,11 +248,12 @@ class _PaymentPageState extends State<PaymentPage> {
                                     });
                                   },
                                   child: Transform.scale(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: ColorOption(c ?? Colors.red),
-                                      ),
-                                      scale: active == c ? 1.2 : 1),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ColorOption(c ?? Colors.red),
+                                    ),
+                                    scale: active == c ? 1.2 : 1,
+                                  ),
                                 ))
                             .toList(),
                       ),
@@ -194,32 +263,34 @@ class _PaymentPageState extends State<PaymentPage> {
                       height: 250,
                       width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: shadow,
-                          borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(10),
-                              bottomLeft: Radius.circular(10))),
+                        color: Colors.white,
+                        boxShadow: shadow,
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                      ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
                           Container(
                             padding: EdgeInsets.only(left: 16.0),
                             decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
                               color: Colors.grey[200],
                             ),
                             child: TextField(
                               inputFormatters: [
-                                LengthLimitingTextInputFormatter(16)
+                                LengthLimitingTextInputFormatter(16),
                               ],
                               controller: cardNumber,
                               onChanged: (val) {
                                 setState(() {});
                               },
                               decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Card Number'),
+                                border: InputBorder.none,
+                                hintText: 'Card Number',
+                              ),
                             ),
                           ),
                           Row(
@@ -234,12 +305,13 @@ class _PaymentPageState extends State<PaymentPage> {
                                   ),
                                   child: TextField(
                                     inputFormatters: [
-                                      LengthLimitingTextInputFormatter(2)
+                                      LengthLimitingTextInputFormatter(2),
                                     ],
                                     controller: month,
                                     decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: 'Month'),
+                                      border: InputBorder.none,
+                                      hintText: 'Month',
+                                    ),
                                     onChanged: (val) {
                                       setState(() {});
                                     },
@@ -259,12 +331,13 @@ class _PaymentPageState extends State<PaymentPage> {
                                   ),
                                   child: TextField(
                                     inputFormatters: [
-                                      LengthLimitingTextInputFormatter(2)
+                                      LengthLimitingTextInputFormatter(2),
                                     ],
                                     controller: year,
                                     decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: 'Year'),
+                                      border: InputBorder.none,
+                                      hintText: 'Year',
+                                    ),
                                     onChanged: (val) {
                                       setState(() {});
                                     },
@@ -285,8 +358,9 @@ class _PaymentPageState extends State<PaymentPage> {
                                   child: TextField(
                                     controller: cvc,
                                     decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: 'CVC'),
+                                      border: InputBorder.none,
+                                      hintText: 'CVC',
+                                    ),
                                     onChanged: (val) {
                                       setState(() {});
                                     },
@@ -298,15 +372,15 @@ class _PaymentPageState extends State<PaymentPage> {
                           Container(
                             padding: EdgeInsets.only(left: 16.0),
                             decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
                               color: Colors.grey[200],
                             ),
                             child: TextField(
                               controller: cardHolder,
                               decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Name on card'),
+                                border: InputBorder.none,
+                                hintText: 'Name on card',
+                              ),
                               onChanged: (val) {
                                 setState(() {});
                               },
@@ -317,10 +391,11 @@ class _PaymentPageState extends State<PaymentPage> {
                     ),
                     SizedBox(height: 24.0),
                     Center(
-                        child: Padding(
-                      padding: EdgeInsets.only(bottom: 20),
-                      child: addThisCard,
-                    ))
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: addThisCard,
+                      ),
+                    )
                   ],
                 ),
               ),
