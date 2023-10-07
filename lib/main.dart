1f0b8c115530ab507dd2cb27fe417a936c40c8b5
William <68315131+SCUMBAG0LEE@users.noreply.github.com>
2023-10-07 15:19:54 +0700

    Updated main.dart for provider addition

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:bad_tech/screens/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Import Provider package
+import 'package:bad_tech/models/cart_provider.dart'; // Import CartProvider
 
-
-
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider( // Wrap your MaterialApp with ChangeNotifierProvider
+      create: (_) => CartProvider(), // Create an instance of CartProvider
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
